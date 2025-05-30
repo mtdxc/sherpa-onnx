a02953a2e52829c0125e980f39f7f16a492ba8d2
mtdxc <mtdxc@126.com>
2025-05-30 21:16:18 +0800

    indent for chrono::milliseconds

--- sherpa-onnx/csrc/sherpa-onnx-offline-language-identification.cc
+++ sherpa-onnx/csrc/sherpa-onnx-offline-language-identification.cc
@@ -94,9 +94,7 @@ for a list of pre-trained models to download.
   fprintf(stdout, "%s\n", language.c_str());
 
   float elapsed_seconds =
-      std::chrono::duration_cast<std::chrono::milliseconds>(end - begin)
-          .count() /
-      1000.;
+      std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / 1000.;
 
   fprintf(stderr, "num threads: %d\n", config.num_threads);
 
