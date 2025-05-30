367834a82c446306de7d6d2ea9b17f4e0e006ecb
cqm <cqm@97kid.com>
2025-05-30 14:27:22 +0800

    resample增加参数缺省值

--- sherpa-onnx/csrc/sherpa-onnx-vad-microphone.cc
+++ sherpa-onnx/csrc/sherpa-onnx-vad-microphone.cc
@@ -104,12 +104,7 @@ wget https://github.com/snakers4/silero-vad/raw/master/src/silero_vad/data/siler
   float sample_rate = 16000;
   std::unique_ptr<sherpa_onnx::LinearResample> resampler;
   if (mic_sample_rate != sample_rate) {
-    float min_freq = std::min(mic_sample_rate, sample_rate);
-    float lowpass_cutoff = 0.99 * 0.5 * min_freq;
-
-    int32_t lowpass_filter_width = 6;
-    resampler = std::make_unique<sherpa_onnx::LinearResample>(
-        mic_sample_rate, sample_rate, lowpass_cutoff, lowpass_filter_width);
+    resampler = std::make_unique<sherpa_onnx::LinearResample>(mic_sample_rate, sample_rate);
   }
 
   auto vad = std::make_unique<sherpa_onnx::VoiceActivityDetector>(config);
