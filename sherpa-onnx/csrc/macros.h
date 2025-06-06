4ea299e01f85374277d1af9581fc848746d60b73
cqm <cqm@97kid.com>
2025-06-06 11:03:13 +0800

    log filename not path

--- sherpa-onnx/csrc/macros.h
+++ sherpa-onnx/csrc/macros.h
@@ -6,6 +6,18 @@
 #define SHERPA_ONNX_CSRC_MACROS_H_
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#ifdef _WIN32
+#define DIR_SEPARATOR '\\'
+#define DIR_SEPARATOR_STR "\\"
+#else
+#define DIR_SEPARATOR '/'
+#define DIR_SEPARATOR_STR "/"
+#endif
+
+#ifndef __FILENAME__
+#define __FILENAME__ (strrchr(DIR_SEPARATOR_STR __FILE__, DIR_SEPARATOR) + 1)
+#endif
 
 #include <utility>
 #if SHERPA_ONNX_ENABLE_WASM
@@ -49,7 +61,7 @@
 #else
 #define SHERPA_ONNX_LOGE(...)                        \
   do {                                               \
-    fprintf(stderr, "%s:%s:%d ", __FILE__, __func__, \
+    fprintf(stderr, "%s:%s:%d ", __FILENAME__, __func__, \
             static_cast<int>(__LINE__));             \
     fprintf(stderr, ##__VA_ARGS__);                  \
     fprintf(stderr, "\n");                           \
